d28a528dc1e8647c2f8fd5c468e00784c6fd53ae
techotaku1 <jsdg1818@gmail.com>
2025-03-08 10:36:59 -0500

    Update base href and vercel configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar.component';
-import { ProfileComponent } from './components/profile.component'; // Ensure this path is correct
+import { ProfileComponent } from './components/profile.component';
 import { SkillsComponent } from './components/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    NavbarComponent,
-    ProfileComponent, // Ensure this component is correctly imported
-    SkillsComponent,
-  ],
+  imports: [RouterOutlet, NavbarComponent, ProfileComponent, SkillsComponent],
   template: `
     <app-navbar></app-navbar>
+    <app-profile></app-profile>
+    <!-- Include ProfileComponent -->
+    <app-skills></app-skills>
+    <!-- Include SkillsComponent -->
     <router-outlet></router-outlet>
   `,
   styles: [],
